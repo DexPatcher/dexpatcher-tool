ce0dc02b39ea3de9e3cee1b0c09db8ba25d8e4b2
Lanchon <lanchon@gmail.com>
2015-03-23 04:54:01 -0300

    Fix whitespace

--- lanchon/dexpatcher/annotation/DexTarget.java
+++ lanchon/dexpatcher/annotation/DexTarget.java
@@ -2,7 +2,7 @@ package lanchon.dexpatcher.annotation;
 
 @DexIgnore
 public final class DexTarget {
-	public static final String STATIC_CONSTRUCTOR= "<clinit>";
+	public static final String STATIC_CONSTRUCTOR = "<clinit>";
 	public static final String CONSTRUCTOR = "<init>";
 	private DexTarget() {}
 }
