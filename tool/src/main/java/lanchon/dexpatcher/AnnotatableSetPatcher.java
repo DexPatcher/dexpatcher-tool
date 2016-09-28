23577f1e1b7d2ca05385e0a78d97331b96cbfbbd
Lanchon <lanchon@gmail.com>
2016-09-28 03:52:06 -0300

    Merge AnnotationBasedPatcher into AnnotatableSetPatcher

--- tool/src/main/java/lanchon/dexpatcher/AnnotatableSetPatcher.java
+++ tool/src/main/java/lanchon/dexpatcher/AnnotatableSetPatcher.java
@@ -1,8 +1,13 @@
 package lanchon.dexpatcher;
 
-import org.jf.dexlib2.iface.Annotatable;
+import java.util.Set;
 
-public abstract class AnnotatableSetPatcher<T extends Annotatable> extends AnnotationBasedPatcher<T> {
+import org.jf.dexlib2.iface.Annotatable;
+import org.jf.dexlib2.iface.Annotation;
+
+import static lanchon.dexpatcher.Logger.Level.*;
+
+public abstract class AnnotatableSetPatcher<T extends Annotatable> extends AbstractPatcher<T> {
 
 	protected AnnotatableSetPatcher(Logger logger, String baseLogPrefix) {
 		super(logger, baseLogPrefix);
@@ -19,12 +24,45 @@ public abstract class AnnotatableSetPatcher<T extends Annotatable> extends Annot
 	// Implementation
 
 	@Override
+	protected void onPatch(String patchId, T patch) throws PatchException {
+
+		Set<? extends Annotation> rawAnnotations = patch.getAnnotations();
+		PatcherAnnotation annotation = PatcherAnnotation.parse(rawAnnotations);
+		if (annotation == null) annotation = new PatcherAnnotation(getDefaultAction(patchId, patch), rawAnnotations);
+
+		onPrepare(patchId, patch, annotation);
+
+		Action action = annotation.getAction();
+		if (isLogging(DEBUG)) log(DEBUG, action.getLabel());
+		switch (action) {
+		case ADD:
+			onAdd(patchId, patch, annotation);
+			break;
+		case EDIT:
+			onEdit(patchId, patch, annotation);
+			break;
+		case REPLACE:
+			onReplace(patchId, patch, annotation);
+			break;
+		case REMOVE:
+			onRemove(patchId, patch, annotation);
+			break;
+		case IGNORE:
+			onIgnore(patchId, patch, annotation);
+			break;
+		default:
+			throw new AssertionError("Unexpected action");
+		}
+
+	}
+
+	// Intermediate Handlers
+
 	protected void onAdd(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {
 		T patched = onSimpleAdd(patch, annotation);
 		addPatched(patchId, patch, patched);
 	}
 
-	@Override
 	protected void onEdit(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {
 		String targetId = getTargetId(patchId, patch, annotation);
 		extendLogPrefix(patchId, targetId, annotation);
@@ -34,7 +72,6 @@ public abstract class AnnotatableSetPatcher<T extends Annotatable> extends Annot
 		addPatched(patchId, patch, patched);
 	}
 
-	@Override
 	protected void onReplace(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {
 		String targetId = getTargetId(patchId, patch, annotation);
 		extendLogPrefix(patchId, targetId, annotation);
@@ -43,7 +80,6 @@ public abstract class AnnotatableSetPatcher<T extends Annotatable> extends Annot
 		addPatched(patchId, patch, patched);
 	}
 
-	@Override
 	protected void onRemove(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {
 		String targetId = getTargetId(patchId, patch, annotation);
 		extendLogPrefix(patchId, targetId, annotation);
@@ -51,11 +87,16 @@ public abstract class AnnotatableSetPatcher<T extends Annotatable> extends Annot
 		onSimpleRemove(patch, annotation, target);
 	}
 
+	protected void onIgnore(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {}
+
 	// Adapters
 
 	protected abstract String getTargetLogPrefix(String targetId, PatcherAnnotation annotation);
 
 	// Handlers
+
+	protected abstract Action getDefaultAction(String patchId, T patch) throws PatchException;
+	protected void onPrepare(String patchId, T patch, PatcherAnnotation annotation) throws PatchException {}
 
 	protected abstract String getTargetId(String patchId, T patch, PatcherAnnotation annotation);
 
