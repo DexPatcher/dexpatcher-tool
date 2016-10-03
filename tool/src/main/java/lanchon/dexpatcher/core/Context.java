a45f826fd811c9be0d4f2bb127c25cb19a18774a
Lanchon <lanchon@gmail.com>
2016-10-03 04:26:55 -0300

    Update copyright

--- tool/src/main/java/lanchon/dexpatcher/core/Context.java
+++ tool/src/main/java/lanchon/dexpatcher/core/Context.java
@@ -1,3 +1,13 @@
+/*
+ * DexPatcher - Copyright 2015, 2016 Rodrigo Balerdi
+ * (GNU General Public License version 3 or later)
+ *
+ * DexPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
 package lanchon.dexpatcher.core;
 
 import lanchon.dexpatcher.core.logger.BasicLogger;
