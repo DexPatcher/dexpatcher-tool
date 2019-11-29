c24bb9d70a6f8b768eaca99300f22a62f1d6b2be
Lanchon <lanchon@gmail.com>
2019-11-29 19:07:08 -0300

    Rfactor into AbstractLoggingRewriter

---
+++ tool/src/main/java/lanchon/dexpatcher/naming/AbstractLoggingRewriter.java
@@ -0,0 +1,81 @@
+/*
+ * DexPatcher - Copyright 2015-2019 Rodrigo Balerdi
+ * (GNU General Public License version 3 or later)
+ *
+ * DexPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
+package lanchon.dexpatcher.naming;
+
+import java.util.HashSet;
+
+import lanchon.dexpatcher.core.logger.Logger;
+import lanchon.dexpatcher.core.util.Label;
+
+import static lanchon.dexpatcher.core.logger.Logger.Level.*;
+
+public abstract class AbstractLoggingRewriter {
+
+	private static final boolean LOG_REWRITTEN_TYPES = false;
+
+	protected abstract class MemberContext {
+
+		protected final String definingClass;
+
+		public MemberContext(String definingClass) {
+			this.definingClass = definingClass;
+		}
+
+		public StringBuilder getMessageHeader() {
+			StringBuilder sb = getBaseMessageHeader();
+			if (definingClass != null) {
+				sb.append("type '").append(Label.fromClassDescriptor(definingClass));
+				if (LOG_REWRITTEN_TYPES) {
+					String rewrittenDefiningClass = getRewrittenDefiningClass();
+					if (!rewrittenDefiningClass.equals(definingClass)) {
+						sb.append("' -> '").append(Label.fromClassDescriptor(rewrittenDefiningClass));
+					}
+				}
+				sb.append("': ");
+			}
+			return sb;
+		}
+
+		protected abstract String getRewrittenDefiningClass();
+
+	}
+
+	private final Logger logger;
+	private final String logPrefix;
+
+	private final HashSet<String> loggedMessages = new HashSet<>();
+
+	protected AbstractLoggingRewriter(Logger logger, String logPrefix) {
+		this.logger = logger;
+		this.logPrefix = logPrefix;
+	}
+
+	public final StringBuilder getBaseMessageHeader() {
+		StringBuilder sb = new StringBuilder();
+		if (logPrefix != null) sb.append(logPrefix).append(": ");
+		return sb;
+	}
+
+	public final boolean isLogging(Logger.Level level) {
+		// NOTE: Logger level is assumed to be constant during renaming.
+		// This is why the call to logger.isLogging() is not synchronized.
+		return level != NONE && logger.isLogging(level);
+	}
+
+	public final void log(Logger.Level level, String message) {
+		if (isLogging(level)) {
+			synchronized (this) {
+				if (loggedMessages.add(message)) logger.log(level, message);
+			}
+		}
+	}
+
+}
--- tool/src/main/java/lanchon/dexpatcher/naming/decoder/DexDecoder.java
+++ tool/src/main/java/lanchon/dexpatcher/naming/decoder/DexDecoder.java
@@ -10,20 +10,14 @@
 
 package lanchon.dexpatcher.naming.decoder;
 
-import java.util.HashSet;
-
 import lanchon.dexpatcher.core.logger.Logger;
-import lanchon.dexpatcher.core.util.Label;
+import lanchon.dexpatcher.naming.AbstractLoggingRewriter;
 import lanchon.dexpatcher.naming.decoder.DexDecoderModule.ItemType;
 
 import org.jf.dexlib2.iface.DexFile;
 import org.jf.dexlib2.rewriter.DexRewriter;
 
-import static lanchon.dexpatcher.core.logger.Logger.Level.*;
-
-public class DexDecoder implements DexDecoderModule.ItemRewriter {
-
-	private static final boolean LOG_DECODED_TYPES = false;
+public class DexDecoder extends AbstractLoggingRewriter implements DexDecoderModule.ItemRewriter {
 
 	public static DexFile decode(DexFile dex, NameDecoder nameDecoder, Logger logger, String logPrefix,
 			Logger.Level infoLevel, Logger.Level errorLevel) {
@@ -31,23 +25,21 @@ public class DexDecoder implements DexDecoderModule.ItemRewriter {
 		return new DexRewriter(new DexDecoderModule(decoder)).rewriteDexFile(dex);
 	}
 
-	private class ErrorHandler implements NameDecoder.ErrorHandler {
+	private final class ErrorHandler extends MemberContext implements NameDecoder.ErrorHandler {
 
-		private final String definingClass;
 		private final ItemType itemType;
 		private final String value;
 
 		public ErrorHandler(String definingClass, ItemType itemType, String value) {
-			this.definingClass = definingClass;
+			super(definingClass);
 			this.itemType = itemType;
 			this.value = value;
 		}
 
 		@Override
 		public void onError(String message, String string, int codeStart, int codeEnd, int errorStart, int errorEnd) {
-			// NOTE: This call to logger.isLogging() is not synchronized.
-			if (errorLevel != NONE && logger.isLogging(errorLevel)) {
-				StringBuilder sb = buildMessage();
+			if (isLogging(errorLevel)) {
+				StringBuilder sb = getMessageHeader();
 				sb.append(message);
 				sb.append(" in '").append(string, codeStart, errorStart)
 						.append("[->]").append(string, errorStart, errorEnd).append("[<-]")
@@ -56,42 +48,32 @@ public class DexDecoder implements DexDecoderModule.ItemRewriter {
 			}
 		}
 
-		public final StringBuilder buildMessage() {
-			StringBuilder sb = new StringBuilder();
-			if (logPrefix != null) sb.append(logPrefix).append(": ");
-			if (definingClass != null) {
-				sb.append("type '").append(Label.fromClassDescriptor(definingClass));
-				if (LOG_DECODED_TYPES) {
-					String decodedDefiningClass = nameDecoder.decode(definingClass);
-					if (!definingClass.equals(decodedDefiningClass)) {
-						sb.append("' -> '").append(Label.fromClassDescriptor(decodedDefiningClass));
-					}
-				}
-				sb.append("': ");
-			}
+		@Override
+		public StringBuilder getMessageHeader() {
+			StringBuilder sb = super.getMessageHeader();
 			sb.append(itemType.label).append(" '").append(formatValue(value)).append("': ");
 			return sb;
 		}
 
-		public final String formatValue(String value) {
+		public String formatValue(String value) {
 			return itemType == ItemType.NAKED_TYPE_NAME ? value.replace('/', '.') : value;
+		}
+
+		@Override
+		protected String getRewrittenDefiningClass() {
+			return nameDecoder.decode(definingClass);
 		}
 
 	}
 
 	private final NameDecoder nameDecoder;
-	private final Logger logger;
-	private final String logPrefix;
 	private final Logger.Level infoLevel;
 	private final Logger.Level errorLevel;
 
-	private final HashSet<String> loggedMessages = new HashSet<>();
-
 	private DexDecoder(NameDecoder nameDecoder, Logger logger, String logPrefix, Logger.Level infoLevel,
 			Logger.Level errorLevel) {
+		super(logger, logPrefix);
 		this.nameDecoder = nameDecoder;
-		this.logger = logger;
-		this.logPrefix = logPrefix;
 		this.infoLevel = infoLevel;
 		this.errorLevel = errorLevel;
 	}
@@ -100,19 +82,12 @@ public class DexDecoder implements DexDecoderModule.ItemRewriter {
 	public String rewriteItem(String definingClass, ItemType itemType, String value) {
 		ErrorHandler errorHandler = new ErrorHandler(definingClass, itemType, value);
 		String decodedValue = nameDecoder.decode(value, errorHandler);
-		if (decodedValue != value && decodedValue != null) {
-			// NOTE: This call to logger.isLogging() is not synchronized.
-			if (infoLevel != NONE && logger.isLogging(infoLevel) && !decodedValue.equals(value)) {
-				StringBuilder sb = errorHandler.buildMessage();
-				sb.append("decoded to '").append(errorHandler.formatValue(decodedValue)).append("'");
-				log(infoLevel, sb.toString());
-			}
+		if (decodedValue != value && isLogging(infoLevel) && !decodedValue.equals(value)) {
+			StringBuilder sb = errorHandler.getMessageHeader();
+			sb.append("decoded to '").append(errorHandler.formatValue(decodedValue)).append("'");
+			log(infoLevel, sb.toString());
 		}
 		return decodedValue;
-	}
-
-	private synchronized void log(Logger.Level level, String message) {
-		if (logger.isLogging(level) && loggedMessages.add(message)) logger.log(level, message);
 	}
 
 }
