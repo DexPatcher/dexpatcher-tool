9158f7a36f54e00cebf702ce6f680250be43954b
Lanchon <lanchon@gmail.com>
2019-12-26 09:49:17 -0300

    Add DexMapping

---
+++ tool/src/main/java/lanchon/dexpatcher/transform/mapper/map/DexMapping.java
@@ -0,0 +1,117 @@
+/*
+ * DexPatcher - Copyright 2015-2019 Rodrigo Balerdi
+ * (GNU General Public License version 3 or later)
+ *
+ * DexPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
+package lanchon.dexpatcher.transform.mapper.map;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import lanchon.dexpatcher.core.util.Id;
+import lanchon.dexpatcher.transform.mapper.map.builder.MapBuilder;
+
+import org.jf.dexlib2.iface.reference.FieldReference;
+import org.jf.dexlib2.iface.reference.MethodReference;
+
+public class DexMapping implements MapBuilder, DexMap {
+
+	protected static class ClassMapping implements MemberMapBuilder {
+
+		protected final String mapping;
+
+		protected final Map<String, String> fieldMappings = new HashMap<>();
+		protected final Map<String, String> methodMappings = new HashMap<>();
+
+		public ClassMapping(String mapping) {
+			this.mapping = mapping;
+		}
+
+		public String getMapping() {
+			return mapping;
+		}
+
+		@Override
+		public void addFieldMapping(String type, String name, String newName) {
+			if (newName == null) throw new NullPointerException("newName");
+			String id = Id.ofField(type, name);
+			String current = fieldMappings.put(id, newName);
+			if (current != null) {
+				fieldMappings.put(id, current);
+				throw new BuilderException("duplicate field mapping");
+			}
+		}
+
+		public String getFieldMapping(String fieldId) {
+			return fieldMappings.get(fieldId);
+		}
+
+		@Override
+		public void addMethodMapping(String[] parameterTypes, String returnType, String name, String newName) {
+			if (newName == null) throw new NullPointerException("newName");
+			String id = Id.ofMethod(Arrays.asList(parameterTypes), returnType, name);
+			String current = methodMappings.put(id, newName);
+			if (current != null) {
+				methodMappings.put(id, current);
+				throw new BuilderException("duplicate method mapping");
+			}
+		}
+
+		public String getMethodMapping(String methodId) {
+			return methodMappings.get(methodId);
+		}
+
+	}
+
+	protected final Map<String, ClassMapping> classMappings = new HashMap<>();
+
+	@Override
+	public MemberMapBuilder addClassMapping(String name, String newName) {
+		if (newName == null) throw new NullPointerException("newName");
+		String id = Id.ofClass(name);
+		ClassMapping classMapping = new ClassMapping(newName);
+		ClassMapping current = classMappings.put(id, classMapping);
+		if (current != null) {
+			classMappings.put(id, current);
+			throw new BuilderException("duplicate type mapping");
+		}
+		return classMapping;
+	}
+
+	@Override
+	public String getClassMapping(String descriptor) {
+		return getClassMappingById(Id.ofClass(descriptor));
+	}
+
+	@Override
+	public String getFieldMapping(FieldReference field) {
+		return getFieldMappingById(Id.ofClass(field.getDefiningClass()), Id.ofField(field));
+	}
+
+	@Override
+	public String getMethodMapping(MethodReference method) {
+		return getMethodMappingById(Id.ofClass(method.getDefiningClass()), Id.ofMethod(method));
+
+	}
+
+	protected final String getClassMappingById(String classId) {
+		return classMappings.get(classId).getMapping();
+	}
+
+	protected final String getFieldMappingById(String classId, String fieldId) {
+		ClassMapping classMapping = classMappings.get(classId);
+		return classMapping != null ? classMapping.getFieldMapping(fieldId) : null;
+	}
+
+	protected final String getMethodMappingById(String classId, String methodId) {
+		ClassMapping classMapping = classMappings.get(classId);
+		return classMapping != null ? classMapping.getMethodMapping(methodId) : null;
+	}
+
+}
---
+++ tool/src/main/java/lanchon/dexpatcher/transform/mapper/map/builder/MapBuilder.java
@@ -0,0 +1,28 @@
+/*
+ * DexPatcher - Copyright 2015-2019 Rodrigo Balerdi
+ * (GNU General Public License version 3 or later)
+ *
+ * DexPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
+package lanchon.dexpatcher.transform.mapper.map.builder;
+
+public interface MapBuilder {
+
+	MemberMapBuilder addClassMapping(String name, String newName);
+
+	interface MemberMapBuilder {
+		void addFieldMapping(String type, String name, String newName);
+		void addMethodMapping(String[] parameterTypes, String returnType, String name, String newName);
+	}
+
+	class BuilderException extends RuntimeException {
+		public BuilderException(String s) {
+			super(s);
+		}
+	}
+
+}
