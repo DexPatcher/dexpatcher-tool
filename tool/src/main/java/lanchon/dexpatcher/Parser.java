01501cb98e8683c334adca5756fbfef4654309ed
Lanchon <lanchon@gmail.com>
2019-12-28 16:15:23 -0300

    Switch to using option builder

--- tool/src/main/java/lanchon/dexpatcher/Parser.java
+++ tool/src/main/java/lanchon/dexpatcher/Parser.java
@@ -187,42 +187,31 @@ public class Parser {
 
 	private static Options addMainOptions(Options options) {
 
-		Option o;
+		options.addOption(Option.builder("o").longOpt("output").hasArg().argName("dex-or-dir").desc("name of output file or directory").build());
 
-		o = new Option("o", "output", true, "name of output file or directory");
-		o.setArgName("dex-or-dir"); options.addOption(o);
+		options.addOption(Option.builder("a").longOpt("api-level").hasArg().argName("n").type(Number.class).desc("android api level (default: auto-detect)").build());
 
-		o = new Option("a", "api-level", true, "android api level (default: auto-detect)");
-		o.setArgName("n"); o.setType(Number.class); options.addOption(o);
+		options.addOption(Option.builder("m").longOpt("multi-dex").desc("enable multi-dex support").build());
+		options.addOption(Option.builder("M").longOpt("multi-dex-threaded").desc("multi-threaded multi-dex (implies: -m)").build());
+		options.addOption(Option.builder("J").longOpt("multi-dex-jobs").hasArg().argName("n").type(Number.class).desc("multi-dex thread count (implies: -m -M) (default: " + "available processors up to " + MultiDexIO.DEFAULT_MAX_THREADS + ")").build());
 
-		options.addOption(new Option("m", "multi-dex", false, "enable multi-dex support"));
-		options.addOption(new Option("M", "multi-dex-threaded", false, "multi-threaded multi-dex (implies: -m)"));
-		o = new Option("J", "multi-dex-jobs", true, "multi-dex thread count (implies: -m -M) (default: " +
-				"available processors up to " + MultiDexIO.DEFAULT_MAX_THREADS + ")");
-		o.setArgName("n"); o.setType(Number.class); options.addOption(o);
+		options.addOption(Option.builder().longOpt("max-dex-pool-size").hasArg().argName("n").type(Number.class).desc("maximum size of dex pools (default: " + DexIO.DEFAULT_MAX_DEX_POOL_SIZE + ")").build());
 
-		o = new Option(null, "max-dex-pool-size", true, "maximum size of dex pools (default: " +
-				DexIO.DEFAULT_MAX_DEX_POOL_SIZE + ")");
-		o.setArgName("n"); o.setType(Number.class); options.addOption(o);
+		options.addOption(Option.builder().longOpt("annotations").hasArg().argName("package").desc("package name of DexPatcher annotations (default: '" + Context.DEFAULT_ANNOTATION_PACKAGE + "')").build());
+		options.addOption(Option.builder().longOpt("no-auto-ignore").desc("no trivial default constructor auto-ignore").build());
 
-		o = new Option(null, "annotations", true, "package name of DexPatcher annotations (default: '" +
-				Context.DEFAULT_ANNOTATION_PACKAGE + "')");
-		o.setArgName("package"); options.addOption(o);
-		options.addOption(new Option(null, "no-auto-ignore", false, "no trivial default constructor auto-ignore"));
+		options.addOption(Option.builder("q").longOpt("quiet").desc("do not output warnings").build());
+		options.addOption(Option.builder("v").longOpt("verbose").desc("output extra information").build());
+		options.addOption(Option.builder("d").longOpt("debug").desc("output debugging information").build());
 
-		options.addOption(new Option("q", "quiet", false, "do not output warnings"));
-		options.addOption(new Option("v", "verbose", false, "output extra information"));
-		options.addOption(new Option("d", "debug", false, "output debugging information"));
+		options.addOption(Option.builder("p").longOpt("path").desc("output relative paths of source code files").build());
+		options.addOption(Option.builder("P").longOpt("path-root").hasArg().argName("root").desc("output absolute paths of source code files").build());
+		options.addOption(Option.builder().longOpt("stats").desc("output timing statistics").build());
 
-		options.addOption(new Option("p", "path", false, "output relative paths of source code files"));
-		o = new Option("P", "path-root", true, "output absolute paths of source code files");
-		o.setArgName("root"); options.addOption(o);
-		options.addOption(new Option(null, "stats", false, "output timing statistics"));
+		options.addOption(Option.builder().longOpt("dry-run").desc("do not write output files (much faster)").build());
 
-		options.addOption(new Option(null, "dry-run", false, "do not write output files (much faster)"));
-
-		options.addOption(new Option(null, "version", false, "print version information and exit"));
-		options.addOption(new Option("?", "help", false, "print this help message and exit"));
+		options.addOption(Option.builder().longOpt("version").desc("print version information and exit").build());
+		options.addOption(Option.builder("?").longOpt("help").desc("print this help message and exit").build());
 
 		return options;
 
@@ -230,51 +219,40 @@ public class Parser {
 
 	private static Options addTransformOptions(Options options) {
 
-		Option o;
+		options.addOption(Option.builder().longOpt("map-source").desc("apply map to identifiers in source").build());
 
-		options.addOption(new Option(null, "map-source", false, "apply map to identifiers in source"));
+		options.addOption(Option.builder().longOpt("unmap-source").desc("apply map inverse to identifiers in source").build());
+		options.addOption(Option.builder().longOpt("unmap-patches").desc("apply map inverse to identifiers in patches").build());
+		options.addOption(Option.builder().longOpt("unmap-output").desc("apply map inverse to identifiers in output").build());
 
-		options.addOption(new Option(null, "unmap-source", false, "apply map inverse to identifiers in source"));
-		options.addOption(new Option(null, "unmap-patches", false, "apply map inverse to identifiers in patches"));
-		options.addOption(new Option(null, "unmap-output", false, "apply map inverse to identifiers in output"));
+		options.addOption(Option.builder().longOpt("map").hasArgs().argName("file").desc("identifier map file (repeatable option)").build());
+		options.addOption(Option.builder().longOpt("invert-map").desc("use inverse of identifier map file").build());
 
-		o = new Option(null, "map", true, "identifier map file (repeatable option)");
-		o.setArgName("file"); options.addOption(o);
-		options.addOption(new Option(null, "invert-map", false, "use inverse of identifier map file"));
+		options.addOption(Option.builder().longOpt("deanon-source").desc("deanonymize anonymous classes in source").build());
+		options.addOption(Option.builder().longOpt("deanon-source-alt").desc("deanonymize source with alternate plan").build());
+		options.addOption(Option.builder().longOpt("deanon-patches").desc("deanonymize anonymous classes in patches").build());
+		options.addOption(Option.builder().longOpt("deanon-patches-alt").desc("deanonymize patches with alternate plan").build());
+		options.addOption(Option.builder().longOpt("reanon-source").desc("reanonymize anonymous classes in source").build());
+		options.addOption(Option.builder().longOpt("reanon-patches").desc("reanonymize anonymous classes in patches").build());
+		options.addOption(Option.builder().longOpt("reanon-output").desc("reanonymize anonymous classes in output").build());
 
-		options.addOption(new Option(null, "deanon-source", false, "deanonymize anonymous classes in source"));
-		options.addOption(new Option(null, "deanon-source-alt", false, "deanonymize source with alternate plan"));
-		options.addOption(new Option(null, "deanon-patches", false, "deanonymize anonymous classes in patches"));
-		options.addOption(new Option(null, "deanon-patches-alt", false, "deanonymize patches with alternate plan"));
-		options.addOption(new Option(null, "reanon-source", false, "reanonymize anonymous classes in source"));
-		options.addOption(new Option(null, "reanon-patches", false, "reanonymize anonymous classes in patches"));
-		options.addOption(new Option(null, "reanon-output", false, "reanonymize anonymous classes in output"));
+		options.addOption(Option.builder().longOpt("main-plan").hasArg().argName("anon-plan").desc("main anonymization plan (default: '" + TypeAnonymizer.DEFAULT_MAIN_ANONYMIZATION_PLAN + "')").build());
+		options.addOption(Option.builder().longOpt("alt-plan").hasArg().argName("anon-plan").desc("alternate plan (default: '" + TypeAnonymizer.DEFAULT_ALTERNATE_ANONYMIZATION_PLAN + "')").build());
+		options.addOption(Option.builder().longOpt("no-reanon-errors").desc("treat reanonymize errors as warnings").build());
 
-		o = new Option(null, "main-plan", true, "main anonymization plan (default: '" +
-				TypeAnonymizer.DEFAULT_MAIN_ANONYMIZATION_PLAN + "')");
-		o.setArgName("anon-plan"); options.addOption(o);
-		o = new Option(null, "alt-plan", true, "alternate plan (default: '" +
-				TypeAnonymizer.DEFAULT_ALTERNATE_ANONYMIZATION_PLAN + "')");
-		o.setArgName("anon-plan"); options.addOption(o);
-		options.addOption(new Option(null, "no-reanon-errors", false, "treat reanonymize errors as warnings"));
+		options.addOption(Option.builder().longOpt("decode-source").desc("decode identifiers in source").build());
+		options.addOption(Option.builder().longOpt("decode-patches").desc("decode identifiers in patches").build());
+		options.addOption(Option.builder().longOpt("decode-output").desc("decode identifiers in output").build());
 
-		options.addOption(new Option(null, "decode-source", false, "decode identifiers in source"));
-		options.addOption(new Option(null, "decode-patches", false, "decode identifiers in patches"));
-		options.addOption(new Option(null, "decode-output", false, "decode identifiers in output"));
+		options.addOption(Option.builder().longOpt("code-marker").hasArg().argName("marker").desc("identifier code marker (default: '" + StringCodec.DEFAULT_CODE_MARKER + "')").build());
+		options.addOption(Option.builder().longOpt("no-decode-errors").desc("treat decode errors as warnings").build());
 
-		o = new Option(null, "code-marker", true, "identifier code marker (default: '" +
-				StringCodec.DEFAULT_CODE_MARKER + "')");
-		o.setArgName("marker"); options.addOption(o);
-		options.addOption(new Option(null, "no-decode-errors", false, "treat decode errors as warnings"));
-
-		StringBuilder sb = new StringBuilder();
+		StringBuilder preTransformSets = new StringBuilder();
 		for (PreTransform pt : PreTransform.values()) {
-			if (sb.length() != 0) sb.append("|");
-			sb.append("'").append(pt.format()).append("'");
+			if (preTransformSets.length() != 0) preTransformSets.append("|");
+			preTransformSets.append("'").append(pt.format()).append("'");
 		}
-		o = new Option(null, "pre-transform", true, "add pre-transform stages (default: '" +
-				Processor.DEFAULT_PRE_TRANSFORM.format() + "') (<set>: " + sb + ")");
-		o.setArgName("set"); options.addOption(o);
+		options.addOption(Option.builder().longOpt("pre-transform").hasArg().argName("set").desc("add pre-transform stages (default: '" + Processor.DEFAULT_PRE_TRANSFORM.format() + "') (<set>: " + preTransformSets + ")").build());
 
 		return options;
 
