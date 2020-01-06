904637bcf271e90f97ad61e7d307d91d497921e8
Lanchon <lanchon@gmail.com>
2020-01-06 09:35:21 -0300

    Test: add test classes into removed package

---
+++ test/patch/src/main/java/test/rec/Rec.java
@@ -0,0 +1,13 @@
+/*
+ * DexPatcher - Copyright 2015-2020 Rodrigo Balerdi
+ * (GNU General Public License version 3 or later)
+ *
+ * DexPatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ */
+
+package test.rec;
+
+public class Rec {}
